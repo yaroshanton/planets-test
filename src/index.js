274ede1099c14d8c78a575db13169918eb906518
Yarosh Anton <on.yaroshanton@gmail.com>
2024-03-18 15:21:20 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//styles
+import './base.css';
+
+const App = () => {
+  return <div className="wrapper">Привіт</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
